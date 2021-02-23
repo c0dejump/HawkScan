dadfb64b4e08596596a401f98f6d9c2fe614f8e4
c0dejump <codejumpgame@gmail.com>
2021-02-23 14:26:03 +0100

    sudo pip removed

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ v_python = "{}".format(sys.version)
 
 if "3." in v_python:
 	#install pip python3
-	os.system("sudo pip3 install requests pyopenssl prettyprinter prettyprint queuelib fake_useragent python-whois argparse bs4 dnspython wafw00f python-whois sockets google")
+	os.system("pip3 install requests pyopenssl prettyprinter prettyprint queuelib fake_useragent python-whois argparse bs4 dnspython wafw00f python-whois sockets")
 elif "2." in v_python:
-	os.system("sudo pip install requests pyopenssl prettyprinter prettyprint queuelib fake_useragent python-whois argparse bs4 dnspython wafw00f python-whois sockets google")
+	os.system("pip install requests pyopenssl prettyprinter prettyprint queuelib fake_useragent python-whois argparse bs4 dnspython wafw00f python-whois sockets")
