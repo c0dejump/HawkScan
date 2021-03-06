86586e49eda7510be8d7fbc6b28bdea039cb9fb4
c0dejump <codejumpgame@gmail.com>
2021-03-06 02:42:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import pathlib
 import setuptools
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
