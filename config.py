aea7291fbe0dbedb671f21e0d0d7e5c3627496a7
c0dejump <codejumpgame@gmail.com>
2021-08-30 13:37:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ JS = "\033[33m[JavaScript] \033[0m"
 WAF = "\033[36m[WAF] \033[0m"
 
 
-#Default abckup extension while your test with the "-b" option
+#Default backup extension while your test with the "-b" option
 EXT_B = ['.db', '.swp', '.yml', '.xsd', '.xml', '.wml', '.bkp', '.rar', '.zip', '.7z', '.bak', '.bac', '.BAK', '.NEW', '.old', 
             '.bkf', '.bok', '.cgi', '.dat', '.ini', '.log', '.key', '.conf', '.env', '_bak', '_old', '.bak1', '.json', '.lock', 
             '.save', '.atom', '.action', '_backup', '.backup', '.config', '?stats=1', '/authorize/', '.md', '.gz', 
